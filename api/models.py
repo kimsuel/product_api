05b0a0663713b03953b40468422afbc85b485b18
slkim <mickycholate@naver.com>
2024-08-26 23:05:18 +0900

    feat: 초기 설정

---
+++ api/models.py
@@ -0,0 +1,25 @@
+import uuid
+
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+
+class Product(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
+
+
+class ShoppingCart(TimeStampedModel):
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter(trailing_slash=False)
+
+
+urlpatterns = [] + router.urls
